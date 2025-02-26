ca495898cbf7e510eaf9b303354e231f91df663f
KenWu0215 <ken2810077@gmail.com>
2025-02-26 08:59:58 +0800

    Elevator 02/26

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Elevator extends SubsystemBase {
+  /** Creates a new Elevator. */
+  SparkMax l_motor = new SparkMax(Constants.CAN_DeviceID.Left_ElevatorID, MotorType.kBrushless);
+  SparkMax r_motor = new SparkMax(Constants.CAN_DeviceID.Right_ElevatorID, MotorType.kBrushless);
+
+  public double UShouldBeHere = 0.0;
+
+  public enum Levels{
+    L1, L2, L3, L4
+  }
+
+  public Elevator() {
+
+  }
+
+  public void SetPower(double n) {
+    l_motor.set(n);
+    r_motor.set(n);
+  }
+
+  public void SetPosition(Levels m_L) {
+    switch (m_L) {
+      case L1:
+
+        l_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        r_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        break;
+
+      case L2:
+
+        l_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        r_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        break;
+
+      case L3:
+
+        l_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        r_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        break;
+
+      case L4:
+
+        l_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        r_motor.getClosedLoopController().setReference(0.0, ControlType.kPosition);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+  public double getPosition() {
+    return (l_motor.getEncoder().getPosition() + r_motor.getEncoder().getPosition()) / 2.0;
+  }
+}
