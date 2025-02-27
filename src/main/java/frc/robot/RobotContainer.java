349db4ff29b6160431158e521c785c6d02cc10a5
Neoxy <neox9487@gmail.com>
2025-02-27 08:41:28 +0800

    add test code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,14 +9,25 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.ClawConstants;
+import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.OperationConstant;
+import frc.robot.commands.claw.SetClawLevel;
+import frc.robot.commands.claw.SetGrabberPower;
+import frc.robot.commands.elevator.SetElevatorHeight;
+import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Swerve;
 import frc.robot.utils.Tools;
 
 public class RobotContainer {
   private Joystick js = new Joystick(0);
+  private Joystick js2 = new Joystick(1);
   
   private Swerve swerve = new Swerve();
+  private Elevator elevator = new Elevator();
+  private Claw claw  = new Claw();
   
   public RobotContainer() {
     swerve.setDefaultCommand(
@@ -32,7 +43,20 @@ public class RobotContainer {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    new JoystickButton(js, 1).onTrue(new SetElevatorHeight(elevator, ElevatorConstants.Levels.L1));
+    new JoystickButton(js, 2).onTrue(new SetElevatorHeight(elevator, ElevatorConstants.Levels.L2));
+    new JoystickButton(js, 3).onTrue(new SetElevatorHeight(elevator, ElevatorConstants.Levels.L3));
+    new JoystickButton(js, 4).onTrue(new SetElevatorHeight(elevator, ElevatorConstants.Levels.L4));
+
+    new JoystickButton(js2, 5).whileTrue(new SetGrabberPower(0.1, claw));
+    new JoystickButton(js2, 5).whileTrue(new SetGrabberPower(-0.1, claw));
+
+    new JoystickButton(js2, 1).onTrue(new SetClawLevel(ClawConstants.Levels.L1, claw));
+    new JoystickButton(js2, 2).onTrue(new SetClawLevel(ClawConstants.Levels.L2, claw));
+    new JoystickButton(js2, 3).onTrue(new SetClawLevel(ClawConstants.Levels.L3, claw));
+    new JoystickButton(js2, 4).onTrue(new SetClawLevel(ClawConstants.Levels.L4, claw));
+  }
 
   public Command getAutonomousCommand() {
     return new PathPlannerAuto("test");
