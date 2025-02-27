64d5334a15f1d729ba6052d6198c6d7396444ffc
yellowcat9487 <yellowcatwhat@gmail.com>
2025-02-27 10:50:50 +0800

    dry

---
+++ src/main/java/frc/robot/utils/AxieOptimizer.java
@@ -0,0 +1,21 @@
+package frc.robot.utils;
+
+public class AxieOptimizer {
+    private final double weight;
+    private double c, t;
+    
+    public AxieOptimizer(double weight) {
+        this.weight = weight;
+        c = 0; t = 0;
+    }
+
+    public double get(double value) {
+        t = value;
+        double err = t - c;
+        if(Math.abs(err) < 0.1 * weight)
+            c = t;
+        else 
+            c += err * weight;
+        return c;
+    }
+}
