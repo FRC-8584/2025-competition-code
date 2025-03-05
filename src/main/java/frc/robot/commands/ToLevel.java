9e3e9cce05f1e6acaa655bb441035122c65134e6
KenWu0215 <ken2810077@gmail.com>
2025-03-05 16:28:33 +0800

    Rick Roll02

--- src/main/java/frc/robot/commands/ToLevel.java
+++ src/main/java/frc/robot/commands/ToLevel.java
@@ -2,44 +2,33 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.ClawConstants;
-import frc.robot.Constants.ElevatorConstants;
-import frc.robot.Constants.OperationConstant.Levels;
+import frc.robot.Constants.Levels;
 import frc.robot.commands.claw.SetClawLevel;
-import frc.robot.commands.elevator.SetElevatorHeight;
+import frc.robot.commands.elevator.SetElevatorLevel;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Elevator;
 
 public class ToLevel extends SequentialCommandGroup {
-    public ToLevel(Levels level, Claw claw, Elevator elevator) {
-        ClawConstants.Levels claw_level = ClawConstants.Levels.L1;
-        ElevatorConstants.Levels elevator_level = ElevatorConstants.Levels.L1;
-        switch (level) {
-            case L1:
-                claw_level = ClawConstants.Levels.L1;
-                elevator_level = ElevatorConstants.Levels.L1;
-                break;
-            case L2:
-                claw_level = ClawConstants.Levels.L2;
-                elevator_level = ElevatorConstants.Levels.L2;
-                break;
-            case L3:
-                claw_level = ClawConstants.Levels.L3;
-                elevator_level = ElevatorConstants.Levels.L3;
-                break;
-            case L4:
-                claw_level = ClawConstants.Levels.L4;
-                elevator_level = ElevatorConstants.Levels.L4;
-                break;
-            default:
-                break;
-        }
-        addCommands(
-            new ParallelCommandGroup(
-                new SetClawLevel(ClawConstants.Levels.L2, claw),
-                new SetElevatorHeight(elevator, elevator_level)
-            ),
-            new SetClawLevel(claw_level, claw)
-        );
+    public ToLevel(Claw claw, Elevator elevator, Levels level) {
+        Levels claw_level = level;
+        Levels elevator_level = level;
+
+        if(level == Levels.DefaultWithAlgae)
+            addCommands(
+                new ParallelCommandGroup(
+                    new SetElevatorLevel(elevator, elevator_level),
+                    new SetClawLevel(level, claw)
+                ),
+                new SetClawLevel(claw_level, claw)
+            );
+        
+        else
+            addCommands(
+                new ParallelCommandGroup(
+                    new SetElevatorLevel(elevator, elevator_level),
+                    new SetClawLevel(Levels.Dodge, claw)
+                ),
+                new SetClawLevel(claw_level, claw)
+            );
     }
 }
